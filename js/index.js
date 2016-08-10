33c6b85aaae8930a4acbfcb6baef757c297fd661
OLucky <elsid1993@gmail.com>
2016-08-10 21:52:00 +0300

    v. 0.95

--- js/index.js
+++ js/index.js
@@ -7,22 +7,23 @@ $(function(){
   $.getJSON("quotes/quotes-list.json", function(data){
     bigList = data;
   });
-
   function getQuote(list){
   seriesNumber = Math.floor(Math.random()*list.Series.length);
   seriesName = list.Series[seriesNumber];
   bookNumber = Math.floor(Math.random()*list[seriesName].Books.length);
   bookName = list[seriesName].Books[bookNumber];
+  $('#book').animate({'opacity':'0'}, 1000, function(){
   $('#book').html(bookName);
+  $('#book').animate({'opacity':'1'}, 1000);
+  });
   var quoteNumber = Math.floor(Math.random()*list[seriesName][bookName].length);
+  $('#quote').animate({'opacity':'0'}, 1000, function(){
   $('#quote').html(list[seriesName][bookName][quoteNumber]);
+  $('#quote').animate({'opacity':'1'}, 1000);
+  });
 }
-
   $('#qbtn').on('click',function(){
     getQuote(bigList);
   });
 
-  $('#vkbtn').on('click', function Share(){
-  
-  });
 });
