2c3db0cdc658736d00156b95318613fe105a6798
OLucky <elsid1993@gmail.com>
2016-08-11 19:44:33 +0300

    1.0
    Added Twitter button
    Visual changes

--- js/index.js
+++ js/index.js
@@ -1,29 +1,47 @@
-$(function(){
-  var bookNumber = "";
-  var bookName = "";
-  var seriesName= "Rincewind";
-  var seriesNumber = "";
-  var bigList = "";
-  $.getJSON("quotes/quotes-list.json", function(data){
-    bigList = data;
-  });
-  function getQuote(list){
-  seriesNumber = Math.floor(Math.random()*list.Series.length);
-  seriesName = list.Series[seriesNumber];
-  bookNumber = Math.floor(Math.random()*list[seriesName].Books.length);
-  bookName = list[seriesName].Books[bookNumber];
-  $('#book').animate({'opacity':'0'}, 1000, function(){
-  $('#book').html(bookName);
-  $('#book').animate({'opacity':'1'}, 1000);
-  });
-  var quoteNumber = Math.floor(Math.random()*list[seriesName][bookName].length);
-  $('#quote').animate({'opacity':'0'}, 1000, function(){
-  $('#quote').html(list[seriesName][bookName][quoteNumber]);
-  $('#quote').animate({'opacity':'1'}, 1000);
-  });
-}
-  $('#qbtn').on('click',function(){
-    getQuote(bigList);
-  });
+$(function() {
+    var bookName = "Wyrd Sisters";
+    var bigList = "";
+    var quote = "Through the fathomless deeps of space swims the star turtle Great A’Tuin, bearing on its back the four giant elephants who carry on their shoulders the mass of the Discworld.";
+    $.getJSON("https://raw.githubusercontent.com/OLucky/discworld.quotes/master/quotes/quotes-list.json", function(data) {
+        bigList = data;
+    });
 
+    //Main function for getting a quote from list
+    function getQuote(list) {
+        var seriesNumber = Math.floor(Math.random() * list.Series.length);
+        var seriesName = list.Series[seriesNumber];
+        var bookNumber = Math.floor(Math.random() * list[seriesName].Books.length);
+        bookName = list[seriesName].Books[bookNumber];
+        $('#book').animate({
+            'opacity': '0'
+        }, 1000, function() {
+            $('#book').html(bookName);
+            $('#book').animate({
+                'opacity': '1'
+            }, 1000);
+        });
+        var quoteNumber = Math.floor(Math.random() * list[seriesName][bookName].length);
+        quote = list[seriesName][bookName][quoteNumber];
+        $('#quote').animate({
+            'opacity': '0'
+        }, 1000, function() {
+            $('#quote').html(quote);
+            $('#quote').animate({
+                'opacity': '1'
+            }, 1000);
+        });
+    }
+
+    //New quote starter
+    $('#qbtn').on('click', function() {
+        getQuote(bigList);
+    });
+
+    //Twittet button script
+    $('#twtbtn').on('click', function() {
+        quote = quote.replace(/<br>/g, '%0D%0A');
+        var url = "http://twitter.com/share";
+        url += "?text=" + quote + "%0D%0A %0D%0A" + "Terry Pratchett." + " " + bookName + ".";
+        window.location.href = url;
+    });
 });
